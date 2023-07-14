246a4f6976ff2712fe8262bdf651086072741335
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-14 15:34:16 -0400

    Add model

---
+++ RxMemo/Model/Memo.swift
@@ -0,0 +1,25 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by Sonic on 14/7/23.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    let insertDate: Date
+    let identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updateContent: String) {
+        self = original
+        self.content = updateContent
+    }
+}
