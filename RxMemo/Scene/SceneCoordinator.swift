b7cd55accf56fda7f3d8f2e852a632587fd1baaf
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-15 00:07:24 -0400

    fixing transition issue list to detail view

--- RxMemo/Scene/SceneCoordinator.swift
+++ RxMemo/Scene/SceneCoordinator.swift
@@ -9,6 +9,12 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+extension UIViewController {
+    var sceneViewConrtoller: UIViewController {
+        return self.children.first ?? self
+    }
+}
+
 class SceneCoordinator: SceneCoordinatorType {
     private let bag = DisposeBag()
     private var window: UIWindow
@@ -27,7 +33,7 @@ class SceneCoordinator: SceneCoordinatorType {
         
         switch style {
         case .root:
-            currentVC = target
+            currentVC = target.sceneViewConrtoller
             window.rootViewController = target
             window.makeKeyAndVisible()
             subject.onCompleted()
@@ -37,14 +43,21 @@ class SceneCoordinator: SceneCoordinatorType {
                 break
             }
             
+            nav.rx.willShow
+                .subscribe(onNext: {[weak self] event in
+                    self?.currentVC = event.viewController.sceneViewConrtoller
+                })
+                .disposed(by: bag)
+            
+            
             nav.pushViewController(target, animated: animated)
-            currentVC = target
+            currentVC = target.sceneViewConrtoller
             subject.onCompleted()
         case .modal:
             currentVC?.present(target, animated: animated) {
                 subject.onCompleted()
             }
-            currentVC = target
+            currentVC = target.sceneViewConrtoller
         }
         
         return subject.ignoreElements().asCompletable()
@@ -55,7 +68,7 @@ class SceneCoordinator: SceneCoordinatorType {
         return Completable.create { [weak self] completable in
             if let presentingVC = self?.currentVC?.presentingViewController {
                 self?.currentVC?.dismiss(animated: animated) {
-                    self?.currentVC = presentingVC
+                    self?.currentVC = presentingVC.sceneViewConrtoller
                     completable(.completed)
                 }
             } else if let nav = self?.currentVC?.navigationController {
